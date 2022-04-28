163c5c3f2839a15eafb857949d17d9ef7a144b3c
Berney Alec <beral@sevjnet.ch>
2022-04-28 09:09:17 +0200

    creating clean models

---
+++ app/Models/old/Message_old.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Message extends Model
+{
+    use HasFactory;
+    //use SoftDeletes;
+    protected $fillable = [
+      'sender_id',
+      'recipient_id',
+      'job_id',
+      'text',
+      'notify'
+    ];
+}
