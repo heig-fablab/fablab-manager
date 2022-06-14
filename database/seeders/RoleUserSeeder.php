fb8ac4c222947e788aa4a68e4a71efe6d7f96805
alecberney <beral@sevjnet.ch>
2022-06-14 12:15:14 +0200

    suppress unused imports

--- database/seeders/RoleUserSeeder.php
+++ database/seeders/RoleUserSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -19,7 +18,7 @@ class RoleUserSeeder extends Seeder
             'user_switch_uuid' => '111@hes-so.ch',
             'role_id' => 1,
         ]);
-        
+
         DB::table('role_user')->insert([
             'user_switch_uuid' => '222@hes-so.ch',
             'role_id' => 2,
