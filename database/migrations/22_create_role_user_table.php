ce07a6e2a1f53598876b2b78d759e2e26d93a358
alecberney <beral@sevjnet.ch>
2022-06-14 12:13:05 +0200

    migrations down v1

--- database/migrations/22_create_role_user_table.php
+++ database/migrations/22_create_role_user_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -32,6 +33,8 @@ return new class extends Migration
      */
     public function down()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
         Schema::dropIfExists('role_user');
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
     }
 };
