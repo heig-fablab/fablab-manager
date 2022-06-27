596b25121595b275473bfff4ec8bb551b574f3b8
Berney Alec <beral@sevjnet.ch>
2022-06-27 11:34:33 +0200

    try to add provider

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        KeycloakGuard\KeycloakGuardServiceProvider::class,
         //StudentAffairsUwm\Shibboleth\ShibbolethServiceProvider::class,
 
         /*
